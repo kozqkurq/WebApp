7bb1fd92293f572362ba3e5b15bc4d1524023578
kozqkurq <tsubasa.k0422@icloud.com>
2022-06-18 13:55:52 +0900

    third

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -17,3 +17,15 @@ def test_request():
 @app.route("/exercise_request/<dummy>")
 def exercise_request(dummy):
     return f"exercise_request:{dummy}"
+
+@app.route("/show_html")
+def show_html():
+    return render_template("test_html.html")
+
+@app.route("/show_exercise_html")
+def show_exercise_html():
+    return render_template("exercise.html")
+
+@app.route("/exercise")
+def exercise():
+    return render_template("answer.html", name=request.args.get("my_name"))
